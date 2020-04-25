1777661c8de21926f657212f047e242ef346a458
xroah <396069583@qq.com>
2020-04-25 15:41:38 +0800

    download the image and set as wallpaper for windows

---
+++ index.js
@@ -0,0 +1,146 @@
+"use strict"
+
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+const Jimp = require("jimp");
+const qs = require("querystring");
+const childProc = require("child_process");
+
+const DATE_SEP = "/";
+
+function handleImageInfo(info) {
+    const image = info.images[0];
+    const hostname = "https://cn.bing.com";
+    const date = new Date();
+    const year = date.getFullYear();
+    const mon = date.getMonth() + 1;
+    const day = date.getDate();
+    const convert = num => (100 + num).toString().substring(1);
+    const dateStr = `${year}${DATE_SEP}${convert(mon)}${DATE_SEP}${convert(day)}`;
+
+    return {
+        date: dateStr,
+        copyright: image.copyright,
+        copyrightUrl: `${hostname}${image.copyrightlink}`,
+        url: `${hostname}${image.url}`,
+        quiz: `${hostname}${image.quiz}`,
+        hash: image.hsh
+    };
+}
+
+function fetchImgInfo(callback) {
+    const now = Date.now();
+    const p = "/HPImageArchive.aspx";
+    const params = [
+        "format=js",
+        "idx=0",
+        "n=1",
+        "nc=" + now,
+        "pid=hp"
+    ];
+    const req = https.request(
+        {
+            hostname: "cn.bing.com",
+            path: `${p}?${params.join("&")}`
+        },
+        res => {
+            let ret = "";
+
+            res.on("data", chunk => ret += chunk);
+            res.on("end", () => {
+                if (typeof callback === "function") {
+                    callback(ret, handleImageInfo(JSON.parse(ret)));
+                }
+            });
+        }
+    );
+
+    req.on("error", err => console.log(err));
+    req.end();
+}
+
+function setWallpaper(img) {
+    if (process.platform === "win32") {
+        //windows api only accepts .bmp img
+        const imgObj = path.parse(img);
+        const bmp = path.join(imgObj.dir, `${imgObj.name}.bmp`);
+
+        Jimp
+            .read(img)
+            .then(data => {
+                //convert to  bmp img
+                return data.write(bmp);
+            })
+            .then(() => {
+                //remove the original image
+                fs.unlinkSync(img);
+                childProc.execFile(
+                    `${__dirname}/widnows/Wallpaper/Debug/Wallpaper.exe`,
+                    [bmp.split(path.sep).join("\\\\")],
+                    {
+                        stdio: "inherit",
+                        shell: true
+                    },
+                    err => {
+                        if (err) throw err;
+
+                        console.log("Success.");
+                    }
+                );
+            })
+            .catch(err => console.log(err));
+    }
+}
+
+function downloadImage(info, handled) {
+    const url = JSON.parse(info).images[0].url;
+    const req = https.request(
+        {
+            hostname: "cn.bing.com",
+            path: url
+        },
+        res => {
+            const id = qs.parse(url.split("?")[1]).id;
+            const mon = path.dirname(handled.date);
+            const infoDest = path.join("images", mon);
+            let dest;
+            if (process.platform === "win32") {
+                dest = path.join("C:/BingWallpaper", mon);
+            } else {
+                dest = path.join("~/BingWallpaper", mon);
+            }
+
+            if (!fs.existsSync(dest)) {
+                fs.mkdirSync(dest, { recursive: true });
+            }
+
+            if (!fs.existsSync(infoDest)) {
+                fs.mkdirSync(infoDest, { recursive: true });
+            }
+
+            const imgPath = path.join(dest, id);
+            const img = fs.createWriteStream(imgPath);
+
+            res.on("end", () => {
+                img.close();
+                setWallpaper(imgPath);
+            });
+            res.pipe(img);
+
+            //write the info
+            fs.writeFileSync(
+                path.join(
+                    infoDest,
+                    handled.date.replace(new RegExp(DATE_SEP, "g"), "-") + ".json"
+                ),
+                JSON.stringify(handled, null, 4)
+            );
+        }
+    );
+
+    req.end();
+}
+fetchImgInfo((raw, handled) => {
+    downloadImage(raw, handled);
+});
---
+++ widnows/Wallpaper/Wallpaper/Wallpaper.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <stdio.h>
+#include <Windows.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 1) {
+		printf("No image provided.\n");
+
+		return 1;
+	}
+	char *img_path = argv[1];
+	wchar_t *w_img_path = 0;
+	int len = MultiByteToWideChar(
+		CP_ACP,
+		0,
+		img_path,
+		strlen(img_path),
+		NULL,
+		0
+	);
+	w_img_path = new wchar_t[len + 1];
+	MultiByteToWideChar(
+		CP_ACP,
+		0,
+		img_path,
+		strlen(img_path),
+		w_img_path,
+		len
+	);
+	w_img_path[len] = '\0';
+	bool ret = SystemParametersInfo(
+		SPI_SETDESKWALLPAPER,
+		0, 
+		(LPWSTR)(w_img_path),
+		SPIF_SENDCHANGE | SPIF_UPDATEINIFILE
+	);
+	printf("%ws\n", w_img_path);
+
+	if (ret) {
+		printf("Success.\n");
+
+		return 0;
+	}
+
+	printf("Failure.\n");
+
+	return 1;
+}
