da682a9ffee986a8faf72c74eb4dd64f61921cf2
xroah <396069583@qq.com>
2022-03-24 22:51:47 +0800

    update pagination, add button component

---
+++ src/client/components/button/index.ts
@@ -0,0 +1,22 @@
+import {defineEl} from "../../utils"
+import template from "./index.html"
+
+class Button extends HTMLElement {
+    constructor() {
+        super()
+
+        const shadow = this.attachShadow({mode: "open"})
+        shadow.innerHTML = template
+    }
+
+    connectedCallback() {
+        const btn = this.shadowRoot!.querySelector(".btn")!
+        const size = this.getAttribute("size")
+        
+        if (size) {
+            btn.classList.add(`btn-${size}`)
+        }
+    }
+}
+
+defineEl("btn-comp", Button)
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,6 @@
 import "./components/home"
 import "./components/pagination"
+import "./components/button"
 
 import "./index.scss"
 
