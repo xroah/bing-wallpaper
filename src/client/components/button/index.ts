82980faa1d8bbe5de4f98ad97e44921f9e8384f8
xroah <396069583@qq.com>
2022-04-18 21:05:12 +0800

    size

--- src/client/components/button/index.ts
+++ src/client/components/button/index.ts
@@ -2,20 +2,37 @@ import {defineEl} from "../../utils"
 import template from "./index.html"
 
 export class Button extends HTMLElement {
+    private _size = ""
+    private _btn: HTMLButtonElement
+
     constructor() {
         super()
 
         const shadow = this.attachShadow({mode: "open"})
         shadow.innerHTML = template
+        this._btn = this.shadowRoot!.querySelector(".btn")!
     }
 
     connectedCallback() {
-        const btn = this.shadowRoot!.querySelector(".btn")!
-        const size = this.getAttribute("size")
-        
-        if (size) {
-            btn.classList.add(`btn-${size}`)
+        this.size = this.getAttribute("size") || ""
+    }
+
+    set size(v: string) {
+        if (v !== this._size) {
+            const prev = `btn-${this._size}`
+
+            if (prev) {
+                this._btn.classList.remove(prev)
+            }
+
+            this._size = v
+
+            this._btn.classList.add(`btn-${v}`)
         }
+    }
+
+    get size() {
+        return this._size
     }
 }
 
